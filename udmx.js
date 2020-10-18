1fc66d5aa1dcff7093af4acdac985436d4e739d1
Bordman-ger <72354455+Bordman-ger@users.noreply.github.com>
2020-10-18 18:39:17 +0200

    Update udmx.js

--- udmx.js
+++ udmx.js
@@ -27,6 +27,7 @@ function DmxAdapter() {
     // private fields
     that._dmxBuffer = null;
     that._udmxDevice = null;
+    that._udmxDeviceList = null;
     
     // register event handlers
     that.adapter.on('ready', function () {
@@ -59,7 +60,15 @@ DmxAdapter.prototype.main = function (allStates) {
         that._udmxDevice = usb.findByIds(vendorId, productId);
         if (!that._udmxDevice) {
             that.adapter.log.error("Couldn't find an NanoDMX USB adapter, not doing anything!");
-              return;
+//Test Devicelist
+            that._udmxDeviceList = usb.getDeviceList();
+            that.adapter.log.warn("Device 1:");
+            that.adapter.log.warn(that._udmxDeviceList[1].deviceDescriptor);
+            that.adapter.log.warn("Device 2:");
+            that.adapter.log.warn(that._udmxDeviceList[2].deviceDescriptor);            
+            
+//        
+            return;
         }
         
         that._udmxDevice.open();
